564ee1f279f6186e7508cac6bf503085f6e45904
Luis David <luisdavidmirandav12@gmail.com>
2025-03-13 09:33:49 -0600

    Proyecto web inicializado

---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MongoProyectoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
