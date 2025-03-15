a5bc31b7ef8cdc67dcb20b76729c60f2d611950f
Luis David <luisdavidmirandav12@gmail.com>
2025-03-15 12:05:56 -0600

    mostrar usuarios web

---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Controllers/UsuariosController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoProyectoWeb.Models;
+
+namespace MongoProyectoWeb.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+        public UsuariosController(IConfiguration configuration, IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Usuarios/0";
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadFromJsonAsync<List<UsuariosModel>>().Result;
+
+                    return View(result);
+                }
+                return View(null);
+
+            }
+        }
+
+
+    }
+}
---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Models/UsuariosModel.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MongoProyectoWeb.Models
+{
+    public class UsuariosModel
+    {
+        public int? _id { get; set; }
+        public string? nombre { get; set; }
+        public string? correo { get; set; }
+
+        public string? contraseña { get; set; } 
+        public string? foto_de_perfil { get; set; }
+    }
+}
