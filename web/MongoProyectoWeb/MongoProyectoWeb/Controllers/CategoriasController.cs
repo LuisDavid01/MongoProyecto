ddc3e588e14544b48fe17d825e464a64f512d752
Luis David <luisdavidmirandav12@gmail.com>
2025-03-16 15:58:28 -0600

    Crud Categorias

---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Controllers/CategoriasController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoProyectoWeb.Models;
+
+namespace MongoProyectoWeb.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public CategoriasController(IConfiguration configuration, IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Categorias/0";
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadFromJsonAsync<List<CategoriasModel>>().Result;
+
+                    return View(result);
+                }
+                return View(null);
+
+            }
+        }
+
+        public IActionResult CrearCategoria()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CrearCategoria(CategoriasModel model)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Categorias";
+                var response = http.PostAsJsonAsync(url, model).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Categorias");
+                }
+                return RedirectToAction("Index", "Categorias");
+
+            }
+        }
+
+        public IActionResult VerCategoria(int id)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Categorias/" + id;
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadFromJsonAsync<CategoriasModel>().Result;
+
+                    return View(result);
+                }
+                return View(null);
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EditarCategoria(CategoriasModel model)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Categorias/" + model._id;
+                var response = http.PutAsJsonAsync(url, model).Result;
+
+                return RedirectToAction("Index", "Categorias");
+
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult EliminarCategoria(int id)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Categorias/" + id;
+                var response = http.DeleteAsync(url).Result;
+
+                return RedirectToAction("Index", "Categorias");
+
+            }
+        }
+    }
+}
