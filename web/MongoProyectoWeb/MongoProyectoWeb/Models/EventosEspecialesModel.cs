02c05f3c0373cbff5048ef0398bc32937926d78f
WendyBadilla <117774092+WendyBadilla@users.noreply.github.com>
2025-04-15 17:44:37 -0600

    Vista de eventos especiales

---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Models/EventosEspecialesModel.cs
@@ -0,0 +1,15 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MongoProyectoWeb.Models
+{
+    public class EventosEspecialesModel
+    {
+        public int? _id { get; set; }
+        public int? id_local { get; set; }
+        public string? nombre_evento { get; set; }
+        public string? descripcion { get; set; }
+        public string? fecha { get; set; }
+        public string? hora { get; set; }
+    }
+}
+
