fa8976185ecbe6867209f4b6f398e8dc696951c7
steven22cr <103520409+steven22cr@users.noreply.github.com>
2025-04-15 20:16:19 -0600

    Ubicaciones en Web
    Agregando el controller, model y index de ubicaciones

---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Controllers/UbicacionesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoProyectoWeb.Models;
+
+namespace MongoProyectoWeb.Controllers
+{
+    public class UbicacionesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public UbicacionesController(IConfiguration configuration, IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Ubicaciones/0";
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadFromJsonAsync<List<UbicacionesModel>>().Result;
+
+                    return View(result);
+                }
+                return View(null);
+
+            }
+        }
+
+        public IActionResult CrearUbicaciones()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CrearUbicaciones(UbicacionesModel model)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Ubicaciones";
+                var response = http.PostAsJsonAsync(url, model).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Ubicaciones");
+                }
+                return RedirectToAction("Index", "Ubicaciones");
+
+            }
+        }
+
+        public IActionResult VerUbicaciones(int id)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Ubicaciones/" + id;
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadFromJsonAsync<UbicacionesModel>().Result;
+
+                    return View(result);
+                }
+                return View(null);
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EditarUbicaciones(UbicacionesModel model)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Ubicaciones/" + model._id;
+                var response = http.PutAsJsonAsync(url, model).Result;
+
+                return RedirectToAction("Index", "Ubicaciones");
+
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult EliminarUbicaciones(int id)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "Ubicaciones/" + id;
+                var response = http.DeleteAsync(url).Result;
+
+                return RedirectToAction("Index", "Ubicaciones");
+
+            }
+        }
+    }
+
+}
---
+++ web/MongoProyectoWeb/MongoProyectoWeb/Models/UbicacionesModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MongoProyectoWeb.Models
+{
+    public class UbicacionesModel : Controller
+    {
+        public int? type { get; set; }
+        public int? id_local { get; set; }
+        public string? direccion { get; set; }
+        public string? provincia { get; set; }
+
+    }
+}
+
