98bf46ce30cba97197efb89a0c4a290a2d2ccbb6
WendyBadilla <117774092+WendyBadilla@users.noreply.github.com>
2025-04-21 23:35:15 -0600

    Correcciones
    Menús, Contactos y Ubicaciones

--- web/MongoProyectoWeb/MongoProyectoWeb/Models/RedesSocialesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MongoProyectoWeb.Models
-{
-    public class RedesSocialesModel
-    {
-        public string? Facebook { get; set; } 
-        public string? Twitter { get; set; }
-        public string? Instagram { get; set; }
-    }
-}
