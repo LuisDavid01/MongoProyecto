7833237de74a51612ab91a623a99aa13ae4372c6
WendyBadilla <117774092+WendyBadilla@users.noreply.github.com>
2025-04-22 01:15:05 -0600

    Cambio tipo de dato

--- web/MongoProyectoWeb/MongoProyectoWeb/Models/VistaResenasModel.cs
+++ web/MongoProyectoWeb/MongoProyectoWeb/Models/VistaResenasModel.cs
@@ -4,6 +4,6 @@
     {
         public int? _id { get; set; }
         public int? total_resenas { get; set; }
-        public int? promedio_calificacion { get; set; }
+        public float? promedio_calificacion { get; set; }
     }
 }
